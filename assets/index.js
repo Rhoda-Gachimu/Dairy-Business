608db090e62d15469ad70e16fda1baa588fc171e
Rhoda <rhoda.gachimu@student.moringaschool.com>
2022-07-19 02:27:27 +0300

    adding functions

--- assets/index.js
+++ assets/index.js
@@ -4,3 +4,8 @@ const shedC = document.getElementById('shed C')
 const shedD = document.getElementById('shed D')
 const Reset = document.getElementsByClassName('button')
 const total  = document.getElementsByClassName("total");
+
+// functions
+ function incomePerMonth(){
+ december.innerHTML = "Income for December is "+dec;
+}
