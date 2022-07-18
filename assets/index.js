75bf0d3385182d6c5c78d0abd23742b91bc61ffc
Rhoda <rhoda.gachimu@student.moringaschool.com>
2022-07-19 01:22:50 +0300

    changed data type to numbers and added functionality to the input placeholders

--- assets/index.js
+++ assets/index.js
@@ -1,1 +1,5 @@
+const shedA = document.getElementById('shed A')
+const shedB= document.getElementById('shed B')
+const shedC = document.getElementById('shed C')
+const shedD = document.getElementById('shed D')
 
