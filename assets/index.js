bbb59a9b6d49d991ebd0afd53dd7d42683902c33
Rhoda <rhoda.gachimu@student.moringaschool.com>
2022-07-19 01:51:31 +0300

    adding functionality to reset and total buttons

--- assets/index.js
+++ assets/index.js
@@ -2,4 +2,5 @@ const shedA = document.getElementById('shed A')
 const shedB= document.getElementById('shed B')
 const shedC = document.getElementById('shed C')
 const shedD = document.getElementById('shed D')
-
+const Reset = document.getElementsByClassName('button')
+const total  = document.getElementsByClassName("total");
